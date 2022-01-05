6b034230463e75f67dda621460020ad3a0275ed2
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-01-05 10:35:24 +0300

    Check if request is auth or not

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
 const jwt = require("jsonwebtoken");
+const utils = require("./utils");
 
 //create an express app instance
 const app = express();
@@ -18,11 +19,76 @@ app.use(bodyparser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyparser.urlencoded({ extended: true }));
 
+// static user details
+const userData = {
+  userId: "1",
+  password: "123456",
+  name: "John Brian",
+  username: "johnbrian",
+  isAdmin: true,
+};
+// Check whether request is authenticated or not
+app.use(function (req, res, nect) {
+  // Check header/url/post params for token
+  let token = req.headers["authorization"];
+
+  // if no token contuinue
+  if (!token) return next();
+
+  token = token.replace("Bearer ", "");
+  jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+    if (err) {
+      return res.status(401).json({
+        error: true,
+        message: "Invalid User.",
+      });
+    } else {
+      // set the user to req so other routes can use it
+      req.user = user;
+      next();
+    }
+  });
+});
 // request handlers
 app.get("/", (req, res) => {
+  if (!req.user)
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid User.\nAccess Denied." });
   res.send("Welcome " + req.user.name);
 });
 
+// validate user credentials
+app.post("users/signin", (req, res) => {
+  const user = req.body.username;
+  const pwd = req.user.password;
+
+  // return 400 status if username/password does not exist
+  if (!user || !pwd) {
+    return res.status(400).json({
+      error: true,
+      message: "Username or Password is required.",
+    });
+  }
+
+  // return 401 status if credentials don't match
+  if (user !== userData.username || pwd !== userData.password) {
+    return res.status(401).json({
+      error: true,
+      message: "Username or Password is wrong",
+    });
+  }
+
+  // generate token
+  const token = utils.generateToken(userData);
+
+  // get basic user details
+  const userObj = utils.getCleanUser(userData);
+
+  // return token along with user details
+  return res.json({ user: userObj, token });
+});
+
 app.listen(port, () => {
-  console.log(`Server Running On Port:${port}`);
+  console.log(`Server Running On Port:${port}.`);
 });
